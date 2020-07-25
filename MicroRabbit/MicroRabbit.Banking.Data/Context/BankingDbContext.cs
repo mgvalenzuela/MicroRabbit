cffe0e62fbb59fb21f67de2f8d53e5338084de3e
mgvalenzuela <50115325+mgvalenzuela@users.noreply.github.com>
2020-07-24 17:34:48 -0700

    Partially completed Banking Microservice

---
+++ MicroRabbit/MicroRabbit.Banking.Data/Context/BankingDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MicroRabbit.Banking.Domain.Models;
+
+namespace MicroRabbit.Banking.Data.Context
+{
+    public class BankingDbContext : DbContext
+    {
+        public BankingDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
